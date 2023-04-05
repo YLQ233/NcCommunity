0594561a8da58f3d0b54ab90c2a7bef336c84e58
YLQ233 <2643635287@qq.com>
2023-04-05 20:54:53 +0800

    Check_Login(Interceptor)

--- src/main/java/com/nc/nccommunity/config/WebMvcConfig.java
+++ src/main/java/com/nc/nccommunity/config/WebMvcConfig.java
@@ -1,5 +1,6 @@
 package com.nc.nccommunity.config;
 
+import com.nc.nccommunity.controller.interceptor.LoginRequiredInterceptor;
 import com.nc.nccommunity.controller.interceptor.LoginTicketInterceptor;
 import com.nc.nccommunity.util.HostHolder;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,11 +11,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 	@Autowired
-	LoginTicketInterceptor loginTicketInterceptor;
+	private LoginTicketInterceptor loginTicketInterceptor;
+	@Autowired
+	private LoginRequiredInterceptor loginRequiredInterceptor;
+	
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(loginTicketInterceptor)
 				.excludePathPatterns("/**/*.css", "/**/*.js", "/**/*.png", "/**/*.jpg", "/**/*.jpeg");
 		
+		registry.addInterceptor(loginRequiredInterceptor)
+				.excludePathPatterns("/**/*.css", "/**/*.js", "/**/*.png", "/**/*.jpg", "/**/*.jpeg");
 	}
 }
